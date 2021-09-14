d94c18c0ffe7789a87686168b811ed0b0d5ba235
Joychoudhury <72333126+Joychoudhury@users.noreply.github.com>
2021-09-14 16:53:06 +0530

    to-do-list  final

---
+++ app.js
@@ -0,0 +1,28 @@
+const { urlencoded } = require("express");
+const express = require("express");
+
+const app = express();
+var items= ["Buy","Cook","Eat"];
+
+app.set("view engine","ejs");
+app.use(urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    var today = new Date;
+    var options ={  weekday: 'long', month: 'long', day: 'numeric' };
+    var day=today.toLocaleString("en-US",options);
+    res.render("list",{nameday: day,newlistitem : items});
+    
+});
+
+app.post("/",function(req,res){
+    var item = req.body.newitem;
+    items.push(item);
+    console.log(item);
+    res.redirect("/");
+});
+
+app.listen(3000,function(){
+    console.log("server started on port 3000");
+});
